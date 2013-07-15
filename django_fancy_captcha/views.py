7cbd50c05813bcd7f2b1c4aac595a6791d5fa346
Florian Finke <flo@randomknowledge.org>
2013-07-15 11:21:14 +0200

    catch IndexError Exception

--- django_fancy_captcha/views.py
+++ django_fancy_captcha/views.py
@@ -1,7 +1,8 @@
 # coding=utf-8
+import logging
 from django_fancy_captcha.utils import create_session_data, session_key, cubes_key, images_key
 from django.views.decorators.cache import never_cache
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 @never_cache
@@ -14,6 +15,10 @@ def image(request, cube_num, image_num, num_cubes):
 
     session_data = request.session[session_key]
     images = session_data[cubes_key][cube_num]
-    img = images[image_num]
+    try:
+        img = images[image_num]
+    except IndexError:
+        logging.warn("Didn't find image number {0} in session: {1}".format(image_num, images))
+        raise Http404
 
     return HttpResponse(session_data[images_key][img], mimetype="image/jpeg")
